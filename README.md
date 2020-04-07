b6b46ee636b49acf9ea4fe565521151a1e9f8f3d
Barad Bhavinkumar <bbhavin.nitsan@gmail.com>
2020-04-07 13:18:40 +0530

    [BUGFIX] Removed unnecessary code & updated yaml for RTE [TODO] Minor release with code-clean-up

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bj_polaroyd
+
+The TYPO3 theme which includes a full configured frontend theme for TYPO3, based on the Bootstrap CSS Framework. 
+Template URL: https://templatemag.com/demo/Polaroyd/
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Polaroyd Website Template',
-	'description' => 'The TYPO3 theme which includes complete template of this website.',
+	'description' => 'The TYPO3 theme which includes a full configured frontend theme for TYPO3, based on the Bootstrap CSS Framework. URL: https://templatemag.com/demo/Polaroyd/',
 	'category' => 'templates',
 	'author' => 'Bhavin Barad',
 	'author_email' => 'baradbhavin2303@gmail.com',
 	'author_company' => '',
 	'state' => 'stable',
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '8.0.0-10.99.99',
